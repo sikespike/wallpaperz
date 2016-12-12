8fe70af2c7c26eaea03e25d3435a932dfff24c9d
sikespike <vkavoun@gmail.com>
2016-12-12 14:00:12 -0500

    building stuff

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var fs = require("fs")
+//var campaignList = require('./campaign-list.js');
+//var campaignData = require('./campaign-data.js');
+var fs = require("fs");
 
 // in NodeJS/Express (server)
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, POST","PUT");
     next();
 });
 
-app.get('/api/loadInventory', function (req, res) {
-    console.log("loadInventory request");
-    console.log("reading inventory.json");
-    var data = fs.readFileSync("inventory.json");
-    //console.log(data);
-    res.send(JSON.parse(data));
-});
+//app.get('/api/campaigns', campaignList);
+//app.get('/api/campaigns/:cid', campaignData);
+
+//---examples of loading json file
+/*app.get('/api/loadInventory', function (req, res) {
+ console.log("loadInventory request");
+ console.log("reading inventory.json");
+ var data = fs.readFileSync("inventory.json");
+ //console.log(data);
+ res.send(JSON.parse(data));
+ });*/
+
+app.use(express.static("dist"));
 
 app.listen(3000, function () {
-    console.log('WallpaperZ Api Server launched on port 3000~!')
+    console.log('WallpaperZ Server launched on port 3000~!')
 });

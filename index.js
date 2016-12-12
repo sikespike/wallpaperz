09e2164f3ed79bc0733164c8b8b53efc65e1b885
sikespike <vkavoun@gmail.com>
2016-12-12 14:09:34 -0500

    finishing configuring initial changes

--- index.js
+++ index.js
@@ -11,19 +11,7 @@ app.all('/*', function(req, res, next) {
     next();
 });
 
-//app.get('/api/campaigns', campaignList);
-//app.get('/api/campaigns/:cid', campaignData);
-
-//---examples of loading json file
-/*app.get('/api/loadInventory', function (req, res) {
- console.log("loadInventory request");
- console.log("reading inventory.json");
- var data = fs.readFileSync("inventory.json");
- //console.log(data);
- res.send(JSON.parse(data));
- });*/
-
-app.use(express.static("dist"));
+app.use(express.static("build"));
 
 app.listen(3000, function () {
     console.log('WallpaperZ Server launched on port 3000~!')
